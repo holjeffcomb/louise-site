2a754dd61f96fa200ba03109c2510eefc0f67743
Jeff Holcomb <jeffh.84@gmail.com>
2024-07-07 19:48:47 -0700

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-6">
-      <div className="flex flex-row gap-6 justify-center items-center m-auto">
+    <main className="flex flex-col items-center justify-between p-6 xl:mx-64 lg:mx-32 md:mx-32 mx-0">
+      <div className="grid gap-6 m-auto justify-center items-center lg:flex lg:flex-row lg:items-start ">
         <Image
           src="/images/lake-bed1.JPG"
           alt="Louise on a dry lake bed"
           width={400}
           height={200}
-          className="rounded-lg"
+          className="rounded-lg w-full md:w-2/3 sm:w-1/2 m-auto"
         />
-        <div>
+        <div className="flex flex-col items-center lg:items-start">
           <h2 className="text-2xl font-lilyScriptOne">Welcome</h2>
-          <p>I'm louise, hello, hi.</p>
+          <p>
+            Platea dictumst quisque sagittis purus sit amet volutpat consequat
+            mauris. Nunc lobortis mattis aliquam faucibus purus in. Vivamus arcu
+            felis bibendum ut tristique et egestas quis. Eget gravida cum sociis
+            natoque penatibus. Senectus et netus et malesuada.
+          </p>
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import { Lily_Script_One } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
